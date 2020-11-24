5a7288fbb9fce9bb33b6e937b6530492884fd5ad
Aibyn <jsaiba301@gmail.com>
2020-11-24 15:25:59 +0600

    Auth timeout increased

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -310,7 +310,7 @@
                     formData.append('iin', this.iin);
                     formData.append('password', this.password)
                     formData.append('platform', 'web')
-                    axios.post('/auth/login', formData, {timeout: 35000})
+                    axios.post('/auth/login', formData, {timeout: 60000})
                     .then((response) => {
                         console.log(response.data)
                         let $response = response.data
